4677d6e568ac360015b8ae82443c1b66f6c745cf
EmranAli-15 <mohammademranali15@gmail.com>
2023-05-19 20:01:06 +0600

    finally success to update data

--- src/components/MyToys/MyToys.jsx
+++ src/components/MyToys/MyToys.jsx
@@ -6,34 +6,31 @@ import Swal from 'sweetalert2';
 const MyToys = () => {
     const [myToys, setMyToys] = useState([]);
     const { user } = useContext(AuthContext);
-    const [update, setUpdate] = useState({});
-    const { _id, price, quantity, details } = update;
-
+    const [toyData, setToyData] = useState({});
+    const { _id, price, quantity, details } = toyData
 
     useEffect(() => {
         fetch(`http://localhost:5000/userToy?email=${user?.email}`)
             .then(res => res.json())
             .then(data => setMyToys(data))
-    }, [user]);
+    }, [user, myToys]);
 
-    const handleEdit = (id) => {
+    const handleSingleId = (id) => {
         fetch(`http://localhost:5000/singleToy/${id}`)
             .then(res => res.json())
-            .then(data => {
-                setUpdate(data);
-                console.log(data);
-            })
+            .then(data => setToyData(data))
     }
 
-    const confirmEdit = (event) => {
+    const handleUpdate = (event) => {
         event.preventDefault()
         const from = event.target;
 
-        const upPrice = from.price.value;
-        const upQuantity = from.quantity.value;
-        const upDetails = from.details.value;
+        const price = from.price.value;
+        const quantity = from.quantity.value;
+        const details = from.details.value;
 
-        const updated = { upPrice, upQuantity, upDetails };
+        const updated = { price, quantity, details }
+        console.log(updated);
 
         Swal.fire({
             title: 'Are you sure?',
@@ -41,7 +38,7 @@ const MyToys = () => {
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, Update it!'
+            confirmButtonText: 'Yes, update it!'
         }).then((result) => {
             if (result.isConfirmed) {
                 fetch(`http://localhost:5000/updateToy/${_id}`, {
@@ -53,6 +50,7 @@ const MyToys = () => {
                 })
                     .then(res => res.json())
                     .then(data => {
+                        console.log(data);
                         if (data.modifiedCount === 1) {
                             Swal.fire(
                                 'Updated!',
@@ -64,7 +62,20 @@ const MyToys = () => {
 
             }
         })
+
+
     }
+
+
+    // const handleEdit = (event) => {
+    //     event.preventDefault()
+    //     const from = event.target;
+    //     const price = from.price.value;
+    //     const quantity = from.quantity.value;
+    //     const details = from.details.value;
+    //     const updated = { price, quantity, details }
+    //     console.log(updated);
+    // }
 
     return (
         <>
@@ -81,7 +92,7 @@ const MyToys = () => {
                     </thead>
                     <tbody>
                         {
-                            myToys.map(toys => <MyToysRow handleEdit={handleEdit} key={toys._id} toys={toys}></MyToysRow>)
+                            myToys.map(toys => <MyToysRow handleSingleId={handleSingleId} key={toys._id} toys={toys}></MyToysRow>)
                         }
                     </tbody>
                 </table>
@@ -89,27 +100,26 @@ const MyToys = () => {
 
             <input type="checkbox" id="my-modal-4" className="modal-toggle" />
             <label htmlFor="my-modal-4" className="modal cursor-pointer">
-                <form onSubmit={confirmEdit} className="modal-box relative" htmlFor="">
-                    <div className="form-control">
-                        <label className="input-group">
-                            <span className='w-32'>Price</span>
-                            <input type="text" name="price" defaultValue={price} className="w-full input input-bordered" />
-                        </label>
-                    </div>
-                    <div className="form-control my-2">
-                        <label className="input-group">
-                            <span className='w-32'>Quantity</span>
-                            <input type="text" name="quantity" defaultValue={quantity} className="w-full input input-bordered" />
-                        </label>
-                    </div>
-                    <textarea className="textarea w-full textarea-bordered" name="details" defaultValue={details}></textarea>
-                    <div className="modal-action">
-                        <label htmlFor="my-modal-4" className="btn btn-sm my-btn-right">
-                            <input type="submit" value="ok" />
-                        </label>
-                    </div>
-
-                </form>
+                <label className="modal-box relative" htmlFor="">
+                    <form onSubmit={handleUpdate}>
+                        <div className="form-control">
+                            <label className="input-group">
+                                <span className='w-32'>Price</span>
+                                <input type="text" name="price" defaultValue={price ? price : ''} className="input input-bordered" />
+                            </label>
+                        </div>
+                        <div className="form-control my-6">
+                            <label className="input-group">
+                                <span className='w-32'>Quantity</span>
+                                <input type="text" name="quantity" defaultValue={quantity ? quantity : ''} className="input input-bordered" />
+                            </label>
+                        </div>
+                        <textarea name="details" defaultValue={details ? details : ''} className="textarea textarea-bordered w-full" placeholder="Bio"></textarea>
+                        <div className='text-right'>
+                            <input type="submit" value="Ok!" />
+                        </div>
+                    </form>
+                </label>
             </label>
         </>
     );
--- src/components/MyToys/MyToysRow.jsx
+++ src/components/MyToys/MyToysRow.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaEdit, FaTrashAlt } from 'react-icons/fa';
 
-const MyToysRow = ({ toys, handleEdit }) => {
+const MyToysRow = ({ toys, handleSingleId }) => {
     const { _id, name, photoURL, price } = toys;
     return (
         <tr>
@@ -18,9 +18,9 @@ const MyToysRow = ({ toys, handleEdit }) => {
             <td>$ {price}</td>
             <td className='md:w-1/4 text-center'>
                 <div>
-                    <button>
+                    <button onClick={() => handleSingleId(_id)}>
                         <label htmlFor="my-modal-4" className="btn bg-transparent border-0 hover:bg-transparent">
-                            <FaEdit onClick={() => handleEdit(_id)} className='text-green-500 h-6 w-7'></FaEdit>
+                            <FaEdit className='text-green-500 h-6 w-7'></FaEdit>
                         </label>
                     </button>
                     <button className="btn btn-ghost btn-xs">
