fb7eabd444dbf37f35cd56b6b11497084acbabaa
EmranAli-15 <mohammademranali15@gmail.com>
2023-05-21 22:26:26 +0600

    see message for not logged users to see details

--- src/Routes/PrivetRoute.jsx
+++ src/Routes/PrivetRoute.jsx
@@ -1,18 +1,29 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Providers/AuthProvider';
 import { Navigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const PrivetRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
     if (loading) {
         return <div className='flex justify-center'>
-        <img src="https://i.ibb.co/6nG56mD/giphy.gif" alt="" />
+            <img src="https://i.ibb.co/6nG56mD/giphy.gif" alt="" />
         </div>
     }
     if (user) {
         return children
     }
-    return <Navigate state={{ from: location }} to="/login" replace></Navigate>;
+    return <div>
+        <Navigate state={{ from: location }} to="/login" replace>
+            {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Please login before go there!',
+                })
+            }
+        </Navigate>
+
+    </div>
 };
 
 export default PrivetRoute;
