755e763f2b6dc26617e5d64283a7a3714443ae1a
EmranAli-15 <mohammademranali15@gmail.com>
2023-05-21 19:01:09 +0600

    loading state updated

--- src/Routes/PrivetRoute.jsx
+++ src/Routes/PrivetRoute.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Providers/AuthProvider';
 import { Navigate } from 'react-router-dom';
-import Loading from '../components/Shared/Loading/Loading';
 
 const PrivetRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
     if (loading) {
-        return <Loading></Loading>
+        return <div className='flex justify-center'>
+        <img src="https://i.ibb.co/6nG56mD/giphy.gif" alt="" />
+        </div>
     }
     if (user) {
         return children
